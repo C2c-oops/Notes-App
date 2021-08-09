0a0304947b946be2ebd15ea45b14df56dc51d10f
Sahil Singh <singhsittu430@gmail.com>
2021-08-09 16:58:40 +0530

    :sparkles: Created function to save notes

--- app/src/main/java/com/c2c/Notes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/c2c/Notes/activities/CreateNoteActivity.java
@@ -2,14 +2,31 @@ package com.c2c.Notes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.c2c.Notes.R;
+import com.c2c.Notes.database.NotesDatabase;
+import com.c2c.Notes.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle;
+    private EditText inputNoteSubtitle;
+    private EditText inputNote;
+
+    private TextView txtDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +35,63 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         ImageView imgBack = findViewById(R.id.imgBack);
         imgBack.setOnClickListener(view -> onBackPressed());
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNote = findViewById(R.id.inputNote);
+
+        txtDateTime = findViewById(R.id.txtDateTime);
+
+        txtDateTime.setText(
+                new SimpleDateFormat(
+                        "EEEE, dd MMMM yyyy HH:mm a",
+                        Locale.getDefault())
+                .format(new Date())
+        );
+
+        ImageView imgSaveNote = findViewById(R.id.imgSave);
+        imgSaveNote.setOnClickListener(view -> saveNote());
     }
+
+    private void saveNote() {
+
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty..!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty()
+                && inputNote.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty..!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Note's Object
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubTitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNote.getText().toString());
+        note.setDateTime(txtDateTime.getText().toString());
+
+        //Async task to save note (Room doesn't allow DB operation on Main Thread)
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getNotesDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
+    }
+
+
 }
